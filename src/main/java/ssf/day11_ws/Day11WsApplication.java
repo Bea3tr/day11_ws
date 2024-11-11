e6db4e93bc832adfe0d0827188ea85a8d497e067
Beatrice <beatricesim31@gmail.com>
2024-11-11 17:39:33 +0800

    spring-boot template

---
+++ src/main/java/ssf/day11_ws/Day11WsApplication.java
@@ -0,0 +1,13 @@
+package ssf.day11_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day11WsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day11WsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssf/day11_ws/Day11WsApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.day11_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day11WsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
