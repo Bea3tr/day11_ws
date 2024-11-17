38768c9962fa170ce73e00f6124a2c4b2aee6922
Beatrice <beatricesim31@gmail.com>
2024-11-17 17:27:57 +0800

    task 3

--- src/main/java/ssf/day11_ws/Day11WsApplication.java
+++ src/main/java/ssf/day11_ws/Day11WsApplication.java
@@ -1,13 +1,39 @@
 package ssf.day11_ws;
 
+import org.springframework.boot.DefaultApplicationArguments;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+/* Task 3
+ * i. mvn clean spring-boot:run -Dspring-boot.run.arguments="--port=3000 --logLevel=TRACE"
+ * ii. in user variables, add SERVER_PORT=3000
+ * iii. in application.properties -> server.port=3000
+ */
 
 @SpringBootApplication
 public class Day11WsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Day11WsApplication.class, args);
-	}
+		SpringApplication app = new SpringApplication(Day11WsApplication.class);
 
+		// Default port #
+		String port = "8080";
+		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+
+		// Port set with command line arguments
+		if(cliOpts.containsOption("port")) {
+			port = cliOpts.getOptionValues("port").get(0);
+		
+			// Set server port # (default or args)
+			app.setDefaultProperties(
+				Collections.singletonMap("server.port", port)
+				);
+		}
+
+		// System.out.printf("Application started on port %s\n", port);
+		app.run(args);
+	}
 }
